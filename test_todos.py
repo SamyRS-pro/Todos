a2f1b1261f1914f6084666690fb1bef67a6a4962
SamyRS-pro <74651695+SamyRS-pro@users.noreply.github.com>
2021-03-20 17:52:24 +0100

    message

--- test_todos.py
+++ test_todos.py
@@ -1,10 +1,15 @@
-from todos import TaskManager, Action
+from todos import TaskManager, Action, Delete
 
 
 def test_can_create_an_action():
     action = Action("add", "first task")
     assert action.name == "add"
     assert action.description == "first task"
+
+def test_can_delete_an_action():
+    action2 = Delete("delete", "second task")
+    assert action2.name2 == "delete"
+    assert action2.description2 == "second task"
 
 
 def test_can_create_a_task_manager():
@@ -17,6 +22,8 @@ def test_task_manager_starts_with_no_tasks():
     assert task_manager.tasks == []
 
 
+
+
 def test_parse_add():
     command = "+ first task"
     task_manager = TaskManager()
@@ -25,3 +32,14 @@ def test_parse_add():
 
     assert action.name == "add"
     assert action.description == "first task"
+
+
+def test_parse_delete():
+    command2 = "- second task"
+    task_manager = TaskManager()
+
+    action2 = task_manager.parse2(command2)
+
+    assert action2.name2 == "delete"
+    assert action2.description2 == "second task"
+
--- todos.py
+++ todos.py
@@ -1,5 +1,5 @@
 class TaskManager:
-    def init(self):
+    def __init__(self):
         self.tasks = []
 
 
@@ -7,8 +7,17 @@ class TaskManager:
         return Action("add", command[2:])
 
 
+    def parse2(self, command):
+        return Delete("delete", command[2:])
+
+
 
 class Action:
-    def init(self, name, description):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
+
+class Delete:
+    def __del__(self, name2, description2):
+        self.name2 = name2
+        self.description2 = description2
